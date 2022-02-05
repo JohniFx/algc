e62ded66083bc12ecf3b201616ddcb48f812b4ba
JohniFx <janos_torok@yahoo.com>
2022-02-05 15:04:31 +0100

    algc updates

--- hello.c
+++ hello.c
@@ -1,8 +1,18 @@
 #include <iostream>
 using namespace std;
+
+/*
+ * Author: Jano
+ * This is just basic cpp practice from different tutorials
+ * 2022-02-05
+ * btw. who is learning cpp these days?!?
+ *
+ * */
  
 int main()
 {
     cout << "C++ cout!" << endl;
     return 0;
 } 
+
+
